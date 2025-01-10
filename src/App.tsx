3ae9e9f7be4c3f2eb08ce0e67c82b218a0c36af2
gustavo.carvalho <gustavo.carvalho@etipi.pi.gov.br>
2025-01-10 12:56:09 -0300

    feat: create project + install styled components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="success"></Button>
+    </>
+  );
+}
